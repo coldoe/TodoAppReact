0986a6fa93e77efc908e7f16bf37d926d02f3c7f
Iceberg <hawxhawx1920@gmail.com>
2020-07-18 16:56:30 +0200

    delete component

--- src/Components/todo-item/todoitem.component.jsx
+++ src/Components/todo-item/todoitem.component.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "../todo-item/todoitem.style.css";
 
-export const Todoitem = (props) => (
-  <div className="todoitem">
-    <h1>{props.description}</h1>
-    <h3>{props.date}</h3>
-    <button onClick={() => console.log(props.id)}>delete</button>
-    <button onClick={() => console.log(props.id)}>update</button>
-  </div>
-);
+export const Todoitem = (props) => {
+  const url = "https://localhost:44367/api/TodoTasks/";
+  const [visible, setVisible] = useState(true);
+
+  async function deleteTodo(id) {
+    const reqOptions = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ idTodoTask: id }),
+    };
+
+    await fetch(url + id, reqOptions)
+      .then((res) => res.json())
+      .then((text) => console.log(text));
+
+    checkStyle();
+  }
+
+  function checkStyle() {
+    visible ? setVisible(false) : setVisible(true);
+  }
+
+  return visible ? (
+    <div className="todoitem">
+      <h1>{props.description}</h1>
+      <h3>{props.date}</h3>
+      <button onClick={() => deleteTodo(props.id)}>delete</button>
+    </div>
+  ) : null;
+};
